906f39d0aed84b7cbd72fede6726a04df698cbb4
Joshua_Masci <iamshortman@gmail.com>
2021-06-14 00:37:30 -0700

    Added main

---
+++ build.zig
@@ -0,0 +1,40 @@
+const builtin = @import("builtin");
+const std = @import("std");
+const Builder = std.build.Builder;
+const Pkg = std.build.Pkg;
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+
+    var exe = b.addExecutable("ZigCraft", "src/main.zig");
+
+    exe.setBuildMode(mode);
+    exe.install();
+
+    exe.addIncludeDir("C:/zig_glfw/include");
+    exe.addLibPath("C:/zig_glfw/build/src/Release");
+    exe.linkSystemLibrary("glfw3");
+
+    exe.linkLibC();
+
+    //OS specific libraries
+    switch (builtin.os.tag) {
+        .windows => {
+            exe.linkSystemLibrary("kernel32");
+            exe.linkSystemLibrary("user32");
+            exe.linkSystemLibrary("shell32");
+            exe.linkSystemLibrary("gdi32");
+        },
+        .linux => {
+            //Add linux libraries if needed
+        },
+        else => {
+            @compileError("Platform not supported, unsure of build requirements");
+        },
+    }
+
+    const play = b.step("run", "Run the engine");
+    const run = exe.run();
+    run.step.dependOn(b.getInstallStep());
+    play.dependOn(&run.step);
+}
