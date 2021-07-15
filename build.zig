b61b38ff4990b09a9a5a53cd029f3ce970920c41
Joshua_Masci <Iamshortman@gmail.com>
2021-07-14 19:19:47 -0700

    Update build.zig

--- build.zig
+++ build.zig
@@ -11,17 +11,10 @@ pub fn build(b: *Builder) void {
     exe.setBuildMode(mode);
     exe.install();
 
-    exe.addIncludeDir("src");
-
     //glfw
     exe.addIncludeDir("C:/zig_glfw/include");
     exe.addLibPath("C:/zig_glfw/build/src/Release");
     exe.linkSystemLibrary("glfw3");
-
-    //vulkan
-    exe.addIncludeDir("C:/VulkanSDK/1.2.162.1/Include");
-    exe.addLibPath("C:/VulkanSDK/1.2.162.1/Lib/");
-    exe.linkSystemLibrary("vulkan-1");
 
     exe.linkLibC();
 
