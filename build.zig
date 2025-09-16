dee10550c612d5b898e8153628a4af291a440d19
Joshua <joshuamasci1@gmail.com>
2025-09-15 17:11:53 -0700

    Removed game project and moved zdxc to its own repo

--- build.zig
+++ build.zig
@@ -4,8 +4,6 @@ const builtin = @import("builtin");
 pub fn build(b: *std.Build) !void {
     const build_sdl3 = b.option(bool, "build-sdl3", "Build and link sdl3 from source instead of using systemlib") orelse false;
     const no_assets = b.option(bool, "no-assets", "Don't compile asset pipeline") orelse false;
-    const no_game = b.option(bool, "no-game", "Don't compile game project") orelse false;
-    _ = no_game; // autofix
     const no_render = b.option(bool, "no-render", "Don't compile render sandbox") orelse false;
 
     const target = b.standardTargetOptions(.{});
@@ -14,10 +12,6 @@ pub fn build(b: *std.Build) !void {
     if (!no_assets) {
         try buildAsset(b, target, optimize);
     }
-
-    // if (!no_game) {
-    //     try buildGame(b, target, optimize, .{ .build_sdl3 = build_sdl3 });
-    // }
 
     if (!no_render) {
         try buildRender(b, target, optimize, .{ .build_sdl3 = build_sdl3 });
@@ -86,76 +80,6 @@ fn buildAsset(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.bui
     run_assets_step.dependOn(&build_game_assets.step);
 }
 
-fn buildGame(
-    b: *std.Build,
-    target: std.Build.ResolvedTarget,
-    optimize: std.builtin.OptimizeMode,
-    options: struct {
-        build_sdl3: bool = false,
-    },
-) !void {
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main_game.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    if (options.build_sdl3) {
-        const sdl3 = b.lazyDependency("sdl", .{
-            .target = target,
-            .optimize = optimize,
-            .preferred_link_mode = .dynamic,
-        }).?;
-        exe_mod.linkLibrary(sdl3.artifact("SDL3"));
-    } else {
-        exe_mod.linkSystemLibrary("SDL3", .{});
-    }
-
-    // vulkan
-    const vulkan_headers = b.dependency("vulkan_headers", .{});
-    const vulkan = b.dependency("vulkan", .{
-        .registry = vulkan_headers.path("registry/vk.xml"),
-    }).module("vulkan-zig");
-    exe_mod.addImport("vulkan", vulkan);
-
-    // dear imgui
-    const zimgui = b.dependency("zimgui", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    exe_mod.addImport("zimgui", zimgui.module("zimgui"));
-
-    // saturn physics abstraction
-    const saturn_jolt = b.dependency("saturn_jolt", .{ .enable_debug_renderer = true });
-    exe_mod.addImport("physics", saturn_jolt.module("root"));
-
-    // zmath
-    const zmath = b.dependency("zmath", .{});
-    exe_mod.addImport("zmath", zmath.module("root"));
-
-    // zlua
-    const zlua = b.dependency("zlua", .{
-        .target = target,
-        .optimize = optimize,
-        .lang = .lua54,
-    });
-    exe_mod.addImport("zlua", zlua.module("zlua"));
-
-    const exe = b.addExecutable(.{
-        .name = "saturn",
-        .root_module = exe_mod,
-    });
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    const run_step = b.step("run-game", "Run the game");
-    run_step.dependOn(&run_cmd.step);
-}
-
 fn buildRender(
     b: *std.Build,
     target: std.Build.ResolvedTarget,
@@ -211,6 +135,6 @@ fn buildRender(
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-    const run_step = b.step("run-render", "Run the render sandbox");
+    const run_step = b.step("run", "Run the render sandbox");
     run_step.dependOn(&run_cmd.step);
 }
